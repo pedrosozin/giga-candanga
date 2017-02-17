bfa4562b604b7e69615bb5751b2c20701693846c
igorsodre <csodreigor@gmail.com>
2017-02-17 20:14:11 -0200

    consetando configuracao da rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'access/home'
+  get 'access/home', controller: "access", action:"home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "access/home"
+  root controller: "access", action:"home"
 end

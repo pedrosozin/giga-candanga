ab7bb6b98c772978e2e94238df8266536ab326ca
renanreboredo <renanrreboredo@gmail.com>
2017-03-03 09:45:06 -0300

    Adicionando autenticação antes de todas as ações e o botão de deslogar na aplicação

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   devise_for :usuarios
 
-
-  # namespace :usuarios do
-  #   get 'edit'
-  #   get 'create'
-  #   get 'new'
-  #   get 'update'
-  #   get 'show'
-  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

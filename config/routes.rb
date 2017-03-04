9324548f1f8d72e47ed77c3cb04361faf33f01b7
renanreboredo <renanrreboredo@gmail.com>
2017-03-04 00:43:59 -0300

    Readicionando rota 'devise_for :usuarios' a routes.rb e consertando comentários no index de usuários

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,6 @@ Rails.application.routes.draw do
     post 'nova_senha', to: 'devise/passwords#create'
   end
   
+  devise_for :usuario, skip: [:registrations, :sessions, :passwords]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

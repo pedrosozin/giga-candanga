fc24ff5a475961ca4f24397b4bf5a2a5e64da2cb
renanreboredo <renanrreboredo@gmail.com>
2017-03-11 21:05:05 -0300

    Adicionando validação de presença para nome e telefone no usuário

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,6 +4,9 @@ class Usuario < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
+  validates :nome, presence: true
+  validates :telefone, presence: true
+
   def self.all_active
     self.where(ativo: true)
   end

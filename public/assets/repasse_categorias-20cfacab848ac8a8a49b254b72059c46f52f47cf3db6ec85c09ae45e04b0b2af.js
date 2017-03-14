680b1eeb9d25f2b0f5a11f45b965c3f22a86663f
igorsodre <csodreigor@gmail.com>
2017-03-14 00:06:37 -0300

    compilando assets para deploy

---
+++ public/assets/repasse_categorias-20cfacab848ac8a8a49b254b72059c46f52f47cf3db6ec85c09ae45e04b0b2af.js
@@ -0,0 +1,42 @@
+function handleNovaRepasseCategoria(event){
+    var dialogRepasseform = $('.dialog-wrapper').html();
+    bootbox.dialog({
+        message: dialogRepasseform,
+        closeButton: false,
+        size: 'large',
+        backdrop: true,
+        onEscape: true
+    });
+}
+
+function handleFormRepasseCategoria(){
+    var options ={
+        rules: {
+            "repasse_categoria[nome]": {
+                required: true,
+                maxlength: 30
+            }
+        },
+        messages: {
+            "repasse_categoria[nome]":{
+                required: "Nome é obrigatório",
+                    maxlength: "Tamanho máximo 30 caracteres"
+            }
+        },
+        errorElement: 'div',
+        errorLabelContainer: '.errorTxt'
+    }; 
+    $('new_repasse_categoria').validate(options);
+}
+
+
+$(document).ready(function(){
+    $('.nova-repasse-categoria').on('click',handleNovaRepasseCategoria);
+    handleFormRepasseCategoria();
+})
+
+// $( document ).on('turbolinks:load', function() {
+//     $('.nova-repasse-categoria').on('click',handleNovaRepasseCategoria);
+//     handleFormRepasseCategoria();
+// })
+;

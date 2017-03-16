eae1b7b9ad99317064d0a641ec26c8fd79e3f926
Marcus Vinícius <marcus.f22@gmail.com>
2017-03-15 23:24:17 -0300

    Issues 66, 62, 61, 60, 59, 56, 55 e 54 verificadas e (quando necessário) corrigidas.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_usuario!
 
-
-
   MSG_TYPE_INFO = "info"
   MSG_TYPE_SUCCESS = "success"
   MSG_TYPE_ERROR = "error"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309144714) do
+ActiveRecord::Schema.define(version: 20170309142817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20170309144714) do
     t.boolean  "ativa",      default: true
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "complementos", force: :cascade do |t|
+    t.string   "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "telefone"
+    t.integer  "usuario_id"
+    t.boolean  "ativo"
+    t.index ["usuario_id"], name: "index_complementos_on_usuario_id", using: :btree
   end
 
   create_table "emails", force: :cascade do |t|
@@ -108,6 +118,7 @@ ActiveRecord::Schema.define(version: 20170309144714) do
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "complementos", "usuarios"
   add_foreign_key "emails", "responsaveis"
   add_foreign_key "enderecos", "instituicoes"
   add_foreign_key "responsaveis", "instituicoes"

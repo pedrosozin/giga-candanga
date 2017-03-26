a2f6102571189759ed87ccd99724c680e927f54d
igorsodre <csodreigor@gmail.com>
2017-03-26 10:45:33 -0300

    correcao no script que roda o rubocop

--- runrubocop.sh
+++ runrubocop.sh
@@ -1,5 +1,5 @@
 # roda rubo cop e tbm o best practices e os abre no navegador
 rubocop -R --format html -o tmp/rubocop.html
 rails_best_practices -f html --output-file tmp/best_practices_output.html
-gnome-open tmp/rubocop.html
-gnome-open tmp/best_practices_output.html
+xdg-open tmp/rubocop.html
+xdg-open tmp/best_practices_output.html
